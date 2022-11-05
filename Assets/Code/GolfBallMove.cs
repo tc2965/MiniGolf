91367919a069511ad58a0bd51d0d294a562c962a
Tiffany Chan <tc2965@nyu.edu>
2022-11-05 14:36:45 -0400

    add game manager
    Co-authored-by: oam114 <oam8118@nyu.edu>

--- Assets/Code/GolfBallMove.cs
+++ Assets/Code/GolfBallMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GolfBallMove : MonoBehaviour
 {
+    public bool paused = false;
     private float scaleDownTouch = 0.3f;
     private Rigidbody _rigidbody;
 
@@ -13,6 +14,12 @@ public class GolfBallMove : MonoBehaviour
 
     void Update()
     {
+        if(!paused) {
+            Time.timeScale = 1;
+        } 
+        else {
+            Time.timeScale = 0;
+        }
         if (Input.touchCount > 0) {
             Touch touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Moved) {
--- Assets/Code/Spikes.cs
+++ Assets/Code/Spikes.cs
@@ -7,16 +7,32 @@ public class Spikes : MonoBehaviour
     private float speed = 1.5f;
     private float yStart;
     private Vector3 _position;
+    private GameManager gameManager;
 
     void Start()
     {
         yStart = transform.position.y;
         _position = transform.position;
+        
+        GameObject gameManagerMaybe = GameObject.FindGameObjectWithTag("GameManager");
+        if (gameManagerMaybe != null) {
+            gameManager = gameManagerMaybe.GetComponent<GameManager>();
+        } else {
+            print("No gamemanager found");
+        }
     }
 
     void Update()
     {
         _position.y = yStart + 2.0f * Mathf.Sin(speed * Time.fixedTime);
         transform.position = _position;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        print("hit something");
+        if (other.gameObject.CompareTag("GolfBall")) {
+            gameManager.RestartLevel();
+        }
     }
 }
