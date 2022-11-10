ad1e9601f88e49323f4e3d0fd8cb4cef4bd1b5cf
Tiffany Chan <tc2965@nyu.edu>
2022-11-10 10:48:30 -0500

    fix lighting and add a next level screen placeholder

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -8,9 +8,10 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject pauseMenu; //must link to settings, restart level, and quit
-    public GameObject settingsMenu; // must have slider for music and sound fx volume & text for their int value
-    public GameObject deathScreen; // must restart level 
+    // public GameObject? pauseMenu; //must link to settings, restart level, and quit
+    // public GameObject? settingsMenu; // must have slider for music and sound fx volume & text for their int value
+    // public GameObject? deathScreen; // must restart level 
+    public GameObject nextLevelScreen; // must restart level 
     public float musicVolume;
     public float soundEffectsVolume;
     [SerializeField] private Slider musicSlider;
@@ -23,11 +24,16 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        if(pauseMenu != null)
-            pauseMenu.SetActive(false);
-        settingsMenu.SetActive(false);
-        if(deathScreen != null)
-            deathScreen.SetActive(false);
+        // if(pauseMenu != null) {
+        //     pauseMenu.SetActive(false);
+        // }
+        // settingsMenu.SetActive(false);
+        // if(deathScreen != null) {
+        //     deathScreen.SetActive(false);
+        // }
+        if(nextLevelScreen != null) {
+            nextLevelScreen.SetActive(false);
+        } 
         Time.timeScale = 1f;
         StartCoroutine(LoadOptions());
         GameObject golfBallMaybe = GameObject.FindGameObjectWithTag("GolfBall");
@@ -40,17 +46,19 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu != null)
-        if (Input.GetButtonDown("Cancel") && pauseMenu != null)
-        {
-            if (pauseMenu.activeInHierarchy)
-            {
-                ClosePauseMenu();
-            } else
-            {
-                ShowPauseMenu();
-            }
+        if (Input.GetKeyDown(KeyCode.Escape) && nextLevelScreen != null) {
+            nextLevelScreen.SetActive(true);
         }
+        // if (Input.GetButtonDown("Cancel") && pauseMenu != null)
+        // {
+        //     if (pauseMenu.activeInHierarchy)
+        //     {
+        //         ClosePauseMenu();
+        //     } else
+        //     {
+        //         ShowPauseMenu();
+        //     }
+        // }
     }
 
     public void StartGame()
@@ -94,44 +102,46 @@ public class GameManager : MonoBehaviour
         soundEffectsMixerGroup.audioMixer.SetFloat("SFX", Mathf.Log10(soundEffectsVolume) * 20);
     }
 
-    public void ShowDeathScreen()
-    {
-        golfBall.paused = true;
-        deathScreen.SetActive(true);
-        //Cursor.lockState = CursorLockMode.None;
-    }
+    // TODO: UNCOMMENT WHEN WE HAVE THE PAUSE AND OPTIONS/SETTING MENUS
 
-    public void ShowPauseMenu()
-    {
-        golfBall.paused = true;
-        pauseMenu.SetActive(true);
-        //Cursor.lockState = CursorLockMode.None;
-    }
+    // public void ShowDeathScreen()
+    // {
+    //     golfBall.paused = true;
+    //     deathScreen.SetActive(true);
+    //     //Cursor.lockState = CursorLockMode.None;
+    // }
 
-    public void ShowSettingsMenu()
-    {
-        settingsMenu.SetActive(true);
-    }
+    // public void ShowPauseMenu()
+    // {
+    //     golfBall.paused = true;
+    //     pauseMenu.SetActive(true);
+    //     //Cursor.lockState = CursorLockMode.None;
+    // }
 
-    public void CloseDeathScreen()
-    {
-        deathScreen.SetActive(false);
-        golfBall.paused = false;
-        //Cursor.lockState = CursorLockMode.Locked;
-    }
-    public void ClosePauseMenu()
-    {
-        settingsMenu.SetActive(false);
-        pauseMenu.SetActive(false);
-        golfBall.paused = false;
-        //Cursor.lockState = CursorLockMode.Locked;
-        SaveOptions();
-    }
-    public void CloseOptionsMenu()
-    {
-        SaveOptions();
-        settingsMenu.SetActive(false);
-    }
+    // public void ShowSettingsMenu()
+    // {
+    //     settingsMenu.SetActive(true);
+    // }
+
+    // public void CloseDeathScreen()
+    // {
+    //     deathScreen.SetActive(false);
+    //     golfBall.paused = false;
+    //     //Cursor.lockState = CursorLockMode.Locked;
+    // }
+    // public void ClosePauseMenu()
+    // {
+    //     settingsMenu.SetActive(false);
+    //     pauseMenu.SetActive(false);
+    //     golfBall.paused = false;
+    //     //Cursor.lockState = CursorLockMode.Locked;
+    //     SaveOptions();
+    // }
+    // public void CloseOptionsMenu()
+    // {
+    //     SaveOptions();
+    //     settingsMenu.SetActive(false);
+    // }
     
     public void RestartLevel()
     {
