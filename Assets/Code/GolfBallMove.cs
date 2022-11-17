2b80b3a42457f310d561894b2d1aad7ee4895dc1
Tiffany Chan <tc2965@nyu.edu>
2022-11-17 17:19:47 -0500

    this is the og ball movement but with a line rendered behind, but it's not accurate

--- Assets/Code/GolfBallMove.cs
+++ Assets/Code/GolfBallMove.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class GolfBallMove : MonoBehaviour
 {
     public bool paused = false;
-    private float scaleDownTouch = 0.3f;
+    private float scaleDownTouch = 1.0f;
     private Rigidbody _rigidbody;
+    private LineRenderer lineRenderer;
     private GameManager gameManager;
-
+    private Vector3 force = Vector3.zero;
+    private bool ballStoppedMoving = true;
 
     private void Start() {
         _rigidbody = GetComponent<Rigidbody>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
 
         GameObject gameManagerMaybe = GameObject.FindGameObjectWithTag("GameManager");
         if (gameManagerMaybe != null) {
@@ -29,20 +33,38 @@ public class GolfBallMove : MonoBehaviour
         else {
             Time.timeScale = 0;
         }
-        if (Input.touchCount > 0) {
+        if (Input.touchCount > 0 && ballStoppedMoving) {
             Touch touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Moved) {
-                Vector3 force = new Vector3(-touch.deltaPosition.x * scaleDownTouch, 0, -touch.deltaPosition.y * scaleDownTouch);
+                force = new Vector3(-touch.deltaPosition.x * scaleDownTouch, 0, -touch.deltaPosition.y * scaleDownTouch);
+                DrawLine(touch);
+            } else if (touch.phase == TouchPhase.Ended) {
                 _rigidbody.AddForce(force);
             }
         }
     }
-
     private void FixedUpdate()
     {
         if (transform.position.y < -30.0f) {
             gameManager.RestartLevel();
         }
+        
+        if (_rigidbody.velocity.magnitude < 0.15f) {
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.angularVelocity = Vector3.zero;
+            ballStoppedMoving = true;
+        } else {
+            lineRenderer.enabled = false;
+        }
+    }
+    private void DrawLine(Touch touch) {
+        float initialDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        Vector3 rayPoint = ray.GetPoint(initialDistance);
+        lineRenderer.enabled = true;
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, new Vector3(rayPoint.x, transform.position.y, rayPoint.z));
     }
 
 }
