a121489b152768b5541699d9fc9433737273fa47
junhkim20 <jk7048@nyu.edu>
2022-11-17 15:49:04 -0500

    Option Section Background Music Continue

--- Assets/Code/Menu.cs
+++ Assets/Code/Menu.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    void Start(){
+        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicClass>().PlayMusic();
+    }
+
     public void GoDemo(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
