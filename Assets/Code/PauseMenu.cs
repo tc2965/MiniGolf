e08f457ce671bf10b0a09f139f26b73ed72e79be
junhkim20 <jk7048@nyu.edu>
2022-11-16 22:35:08 -0500

    Pause Menu Button Prefab & added to all levels

---
+++ Assets/Code/PauseMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PauseMenu : MonoBehaviour
+{
+
+    [SerializeField] GameObject pauseMenu; 
+    public GameObject golf;
+
+    public void Pause(){
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f; 
+        golf = GameObject.FindGameObjectWithTag("GolfBall");
+        GolfBallMove script = golf.GetComponent<GolfBallMove>();
+        script.paused = true; 
+    }
+
+    public void Resume(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f; 
+        golf = GameObject.FindGameObjectWithTag("GolfBall");
+        GolfBallMove script = golf.GetComponent<GolfBallMove>();
+        script.paused = false;
+    }
+
+    public void Home(int sceneID){
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(sceneID);
+    }
+}
