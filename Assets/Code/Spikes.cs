efe184b25cd38b74a1f82fce0834ab2a5785d8db
oam114 <oam8118@nyu.edu>
2022-12-08 07:57:08 -0500

    explosion effect for spike death

--- Assets/Code/Spikes.cs
+++ Assets/Code/Spikes.cs
@@ -8,6 +8,9 @@ public class Spikes : MonoBehaviour
     private float yStart;
     private Vector3 _position;
     private GameManager gameManager;
+    private GameObject golfBall;
+
+    public GameObject explosionEffect;
 
     private void Awake() {
         yStart = transform.position.y;
@@ -32,7 +35,16 @@ public class Spikes : MonoBehaviour
     {
         if (other.gameObject.CompareTag("GolfBall")) {
             Handheld.Vibrate();
-            gameManager.RestartLevel();
+            StartCoroutine(SpikeDeath(other.gameObject.transform.position));
         }
+    }
+
+    IEnumerator SpikeDeath(Vector3 golfBall_pos) {
+        golfBall = GameObject.FindGameObjectWithTag("GolfBall");
+        Instantiate(explosionEffect, golfBall_pos, Quaternion.identity);
+        Destroy(golfBall);
+        yield return new WaitForSeconds(0.5f);
+        print("Death by spikes!!!");
+        gameManager.RestartLevel();
     }
 }
